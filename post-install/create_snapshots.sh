d77b9afedfba78d6d5bd025a3426262653481b0e
Tachi Koma <1080430+greygoo@users.noreply.github.com>
2023-03-20 03:14:29 +0100

    Create create_snapshots.sh

---
+++ post-install/create_snapshots.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+LOOP_DEV=$(losetup -f)
+trap losetup -d ${LOOP_DEV}INT
+
+echo "Mounting built image"
+losetup -P ${LOOP_DEV} output/images/Armbian_23.05.0-trunk_Orangepi3-lts_jammy_current_6.1.15_minimal.img
+mount ${LOOP_DEV}p2 /opt/armbian/
+
+echo "Moving /home to a subvolume"
+mv /opt/armbian/home/ /opt/armbian/home.orig
+btrfs subvolume create /opt/armbian/home/
+mv /opt/armbian/home.orig/* /opt/armbian/home/
+
+echo "Adding entry for subvolume /home to /etc/fstbab"
+HOME_ID=`btrfs subvolume list /opt/armbian/ | egrep "home$" | awk '{ print $2 }'`
+LABEL=`cat /opt/armbian/etc/fstab | grep " / " | awk ' { print $1 } ' | sed "s/UUID=//"`
+sed -i "2i UUID=$LABEL /home btrfs defaults,noatime,commit=600,subvolid=$HOME_ID 0 1" /opt/armbian/etc/fstab
+
+echo "Create snapshot dirctories" 
+mkdir /opt/armbian/.snapshots
+mkdir /opt/armbian/home/.snapshots
+
+echo "Create snapshots for / and /home"
+btrfs subvolume snapshot /opt/armbian/home/ /opt/armbian/home/.snapshots/initial
+btrfs subvolume snapshot /opt/armbian /opt/armbian/.snapshots/initial
+
+btrfs subvolume list /opt/armbian/
+
+echo "Unmounting image"
+umount /opt/armbian
+losetup -d ${LOOP_DEV}
