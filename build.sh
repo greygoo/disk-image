0e24ec74a427ffa67951b27add23689b3f71c32c
Grey Goo <greygoo@hno3.eu>
2023-03-20 20:29:09 +0000

    Remove some options for testing

--- build.sh
+++ build.sh
@@ -9,23 +9,22 @@ echo ${BOARD}
 
 ./compile.sh \
   BOARD=${BOARD} \
-  DISABLE_IPV6=false \
   BRANCH=current \
   RELEASE=jammy \
   BUILD_MINIMAL=yes \
   BUILD_DESKTOP=no \
   BUILD_ONLY=default \
   ROOTFS_TYPE=btrfs \
-  KERNEL_CONFIGURE=no \
-  KERNEL_KEEP_CONFIG=no \
   EXTRAWIFI=yes \
-  EXTERNAL=yes \
-  EXTERNAL_NEW=compile \
-  INSTALL_HEADERS=yes \
-  INSTALL_KSRC=yes \
-  CONSOLE_AUTOLOGIN=yes \
+  CONSOLE_AUTOLOGIN=no \
   PREFER_DOCKER=yes \
-  CLEAN_LEVEL=cache \
+  CLEAN_LEVEL=cache,make,images \
   COMPRESS_OUTPUTIMAGE=sha,gpg,im | tee ${LOG}
 
-./post-build/create_snapshots.sh
+./userpatches/post-build/create_snapshots.sh
+
+#  DISABLE_IPV6=false \
+#  EXTERNAL=yes \
+#  EXTERNAL_NEW=compile \
+#  INSTALL_HEADERS=yes \
+#  INSTALL_KSRC=yes \
