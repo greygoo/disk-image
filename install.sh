d3016b073883256b904a892c9325d6d5e85d9eb3
Grey Goo <greygoo@hno3.eu>
2023-03-21 16:06:06 +0000

    Only clone/patch build repo if not already existing

--- install.sh
+++ install.sh
@@ -4,28 +4,33 @@ BRANCH=${1:-"v23.02"}
 
 echo "Using branch ${BRANCH}. If you like to build the latest armbian, run: $0 main"
 
-echo "Cloning Armbian build system"
-git clone --branch=${BRANCH} https://github.com/armbian/build
+if [ ! -d build ]; then
+    echo "Cloning Armbian build system"
+    git clone --branch=${BRANCH} https://github.com/armbian/build
 
-echo "Patching build system"
-# For 5.15 kernel (old build system)
-if [ ${BRANCH} == "v23.02" ]; then
-  pushd build
-  # Enable 8188 EUS WiFi when builing the image. Requires kernel recompilation
-  patch -p1 < ../patch/0000_enable_8188EUS.patch
-  popd
-fi 
+    echo "Patching build system"
+    # For 5.15 kernel (old build system)
+    if [ ${BRANCH} == "v23.02" ]; then
+        pushd build
+        # Enable 8188 EUS WiFi when builing the image. Requires kernel recompilation
+        patch -p1 < ../patch/0000_enable_8188EUS.patch
+        popd
+    fi 
 
-# For 6.1 kernel (new build system)
-if [ ${BRANCH} == "main" ]; then
-  pushd build
-  # Add patches here 
+    # For 6.1 kernel (new build system)
+    if [ ${BRANCH} == "main" ]; then
+      pushd build
+      # Add patches here 
 
-  popd
+      popd
+  fi
+else
+    echo "build directory exists, only updating userpatches"
 fi
 
-echo "Copying configuration"
+echo "Adding userpatches"
+rm -rf build/userpatches
 cp -r userpatches build
 
-echo "Copying build script"
+echo "Adding build script"
 cp build.sh build
