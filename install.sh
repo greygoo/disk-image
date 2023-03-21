a30e0bdc5ed877c8220946a89ce11782b6ab1f35
Grey Goo <greygoo@hno3.eu>
2023-03-21 03:13:46 +0000

    Make installer patch build system to enable 8188 USB WiFi

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ echo "Using branch ${BRANCH}. If you like to build the latest armbian, run: $0 m
 echo "Cloning Armbian build system"
 git clone --branch=${BRANCH} https://github.com/armbian/build
 
+echo "Patching build syste"
+pushd build
+patch -p1 < ../patch/0000_enable_8188EUS.patch
+popd 
+
 echo "Copying configuration"
 cp -r userpatches build
 
