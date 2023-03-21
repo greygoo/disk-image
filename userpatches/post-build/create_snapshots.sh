86994d8841b5084c7cf77942ce4f987e6de1b0c2
Grey Goo <greygoo@hno3.eu>
2023-03-21 03:31:15 +0000

    Check for image before running and use /tmp instead os /opt

--- userpatches/post-build/create_snapshots.sh
+++ userpatches/post-build/create_snapshots.sh
@@ -2,30 +2,36 @@
 
 
 LOOP_DEV=$(losetup -f)
-trap "losetup -d ${LOOP_DEV}" INT
+
+if [ ! -f output/images/Armbian*.img ]; then
+  echo "No image file found, exiting"
+fi
+
+trap "losetup -d ${LOOP_DEV}" INT TERM
+mkdir /tmp/armbian
 
 echo "Mounting built image"
 losetup -P ${LOOP_DEV} output/images/Armbian*.img
-mount ${LOOP_DEV}p2 /opt/armbian/
+mount ${LOOP_DEV}p2 /tmp/armbian/
 
 echo "Moving /home to a subvolume"
-mv /opt/armbian/home/ /opt/armbian/home.orig
-btrfs subvolume create /opt/armbian/home/
-mv /opt/armbian/home.orig/* /opt/armbian/home/
-rmdir /opt/armbian/home.orig
+mv /tmp/armbian/home/ /tmp/armbian/home.orig
+btrfs subvolume create /tmp/armbian/home/
+mv /tmp/armbian/home.orig/* /tmp/armbian/home/
+rmdir /tmp/armbian/home.orig
 
 echo "Adding entry for subvolume /home to /etc/fstbab"
-HOME_ID=`btrfs subvolume list /opt/armbian/ | egrep "home$" | awk '{ print $2 }'`
-LABEL=`cat /opt/armbian/etc/fstab | grep " / " | awk ' { print $1 } ' | sed "s/UUID=//"`
-sed -i "2i UUID=$LABEL /home btrfs defaults,noatime,commit=600,subvolid=$HOME_ID 0 1" /opt/armbian/etc/fstab
+HOME_ID=`btrfs subvolume list /tmp/armbian/ | egrep "home$" | awk '{ print $2 }'`
+LABEL=`cat /tmp/armbian/etc/fstab | grep " / " | awk ' { print $1 } ' | sed "s/UUID=//"`
+sed -i "2i UUID=$LABEL /home btrfs defaults,noatime,commit=600,subvolid=$HOME_ID 0 1" /tmp/armbian/etc/fstab
 
 echo "Create snapshots for / and /home"
-btrfs subvolume create /opt/armbian/.snapshots
-btrfs subvolume snapshot /opt/armbian/home /opt/armbian/.snapshots/restore_home
-btrfs subvolume snapshot /opt/armbian /opt/armbian/.snapshots/restore_root
+btrfs subvolume create /tmp/armbian/.snapshots
+btrfs subvolume snapshot /tmp/armbian/home /tmp/armbian/.snapshots/restore_home
+btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/restore_root
 
-btrfs subvolume list /opt/armbian
+btrfs subvolume list /tmp/armbian
 
 echo "Unmounting image"
-umount /opt/armbian
+umount /tmp/armbian
 losetup -d ${LOOP_DEV}
