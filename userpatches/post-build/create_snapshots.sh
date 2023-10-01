27d74190d36b770913becb266648253f262e634d
greygoo <greygoo@hno3.eu>
2023-10-01 22:54:27 +0200

    Change snapshot to only be root for now.

--- userpatches/post-build/create_snapshots.sh
+++ userpatches/post-build/create_snapshots.sh
@@ -15,21 +15,28 @@ echo "Mounting built image"
 losetup -P ${LOOP_DEV} output/images/Armbian*.img
 mount ${LOOP_DEV}p2 /tmp/armbian/
 
-echo "Moving /home to a subvolume"
-mv /tmp/armbian/home/ /tmp/armbian/home.orig
-btrfs subvolume create /tmp/armbian/home/
-mv /tmp/armbian/home.orig/* /tmp/armbian/home/
-rmdir /tmp/armbian/home.orig
+#echo "Moving /home to a subvolume"
+#mv /tmp/armbian/home/ /tmp/armbian/home.orig
+#btrfs subvolume create /tmp/armbian/home/
+#mv /tmp/armbian/home.orig/* /tmp/armbian/home/
+#rmdir /tmp/armbian/home.orig
 
-echo "Adding entry for subvolume /home to /etc/fstbab"
-HOME_ID=`btrfs subvolume list /tmp/armbian/ | egrep "home$" | awk '{ print $2 }'`
-LABEL=`cat /tmp/armbian/etc/fstab | grep " / " | awk ' { print $1 } ' | sed "s/UUID=//"`
-sed -i "2i UUID=$LABEL /home btrfs defaults,noatime,commit=600,subvolid=$HOME_ID 0 1" /tmp/armbian/etc/fstab
+#echo "Adding entry for subvolume /home to /etc/fstbab"
+#HOME_ID=`btrfs subvolume list /tmp/armbian/ | egrep "home$" | awk '{ print $2 }'`
+#LABEL=`cat /tmp/armbian/etc/fstab | grep " / " | awk ' { print $1 } ' | sed "s/UUID=//"`
+#sed -i "2i UUID=$LABEL /home btrfs defaults,noatime,commit=600,subvolid=$HOME_ID 0 1" /tmp/armbian/etc/fstab
 
 echo "Create snapshots for / and /home"
 btrfs subvolume create /tmp/armbian/.snapshots
-btrfs subvolume snapshot /tmp/armbian/home /tmp/armbian/.snapshots/restore_home
-btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/restore_root
+btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/root_factory
+btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/root_current
+
+current_root_id=$(btrfs sub list /tmp/armbian/ | grep root_current | awk '{ print $2 }')
+btrfs sub set-default $current_root_id /tmp/armbian
+#btrfs subvolume snapshot /tmp/armbian/home /tmp/armbian/.snapshots/restore_home
+#btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/current_root
+#btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/restore_root
+
 
 btrfs subvolume list /tmp/armbian
 
