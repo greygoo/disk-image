4ffc2e1cd39ea200ebd8b94d0fc6f486d99a4e8d
greygoo <greygoo@hno3.eu>
2023-10-02 02:56:26 +0200

    Add back home subvolume to postinstall

--- userpatches/post-build/create_snapshots.sh
+++ userpatches/post-build/create_snapshots.sh
@@ -15,29 +15,31 @@ echo "Mounting built image"
 losetup -P ${LOOP_DEV} output/images/Armbian*.img
 mount ${LOOP_DEV}p2 /tmp/armbian/
 
-#echo "Moving /home to a subvolume"
-#mv /tmp/armbian/home/ /tmp/armbian/home.orig
-#btrfs subvolume create /tmp/armbian/home/
-#mv /tmp/armbian/home.orig/* /tmp/armbian/home/
-#rmdir /tmp/armbian/home.orig
-
-#echo "Adding entry for subvolume /home to /etc/fstbab"
-#HOME_ID=`btrfs subvolume list /tmp/armbian/ | egrep "home$" | awk '{ print $2 }'`
-#LABEL=`cat /tmp/armbian/etc/fstab | grep " / " | awk ' { print $1 } ' | sed "s/UUID=//"`
-#sed -i "2i UUID=$LABEL /home btrfs defaults,noatime,commit=600,subvolid=$HOME_ID 0 1" /tmp/armbian/etc/fstab
+echo "Moving /home to a subvolume"
+mv /tmp/armbian/home/ /tmp/armbian/home.orig
+btrfs subvolume create /tmp/armbian/home/
+mv /tmp/armbian/home.orig/* /tmp/armbian/home/
+rmdir /tmp/armbian/home.orig
 
 echo "Create snapshots for / and /home"
 btrfs subvolume create /tmp/armbian/.snapshots
+
 btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/root_factory
 btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/root_current
 
+btrfs subvolume snapshot /tmp/armbian/home /tmp/armbian/.snapshots/home_factory
+btrfs subvolume snapshot /tmp/armbian/home /tmp/armbian/.snapshots/home_current
+
+echo Setting root id
 current_root_id=$(btrfs sub list /tmp/armbian/ | grep root_current | awk '{ print $2 }')
 btrfs sub set-default $current_root_id /tmp/armbian
-#btrfs subvolume snapshot /tmp/armbian/home /tmp/armbian/.snapshots/restore_home
-#btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/current_root
-#btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/restore_root
 
+echo "Adding entry for subvolume /home to /etc/fstab"
+current_home_id=$(btrfs sub list /tmp/armbian/ | grep home_current | awk '{ print $2 }')
+label=`cat /tmp/armbian/etc/fstab | grep " / " | awk ' { print $1 } ' | sed "s/UUID=//"`
+sed -i "2i UUID=$label /home btrfs defaults,noatime,commit=600,subvolid=$current_home_id 0 1" /tmp/armbian/etc/fstab
 
+echo "Finale subvolume setup:"
 btrfs subvolume list /tmp/armbian
 
 echo "Unmounting image"
